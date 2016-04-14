b28f7b8af1ef0ea833ed4e48d303b180085774a2
tchiotludo <tchiot.ludo@gmail.com>
2016-04-14 17:24:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Сáша Uri
+-----
+
+## Warning
+Be aware that this package is still in heavy developpement.
+Some breaking change will occure. Thank's for your comprehension.
+
+## Features
+* Url parser
+
+## About
+
+### License
+
+Cawa is licensed under the GPL v3 License - see the `LICENSE` file for details
---
+++ src/HttpClientFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Cache;
+
+use Cawa\Core\DI;
+use Cawa\HttpClient\HttpClient;
+
+trait HttpClientFactory
+{
+    /**
+     * @var array
+     */
+    private static $container = [];
+
+    /**
+     * @param string $name
+     *
+     * @return Cache
+     */
+    private static function httpClient(string $name = null) : Cache
+    {
+        if ($return = DI::get(__METHOD__,  $name)) {
+            return $return;
+        }
+
+        $config = DI::config()->get('httpclient/' . $name);
+
+        if (is_callable($config)) {
+            $item = $config();
+        } else {
+            $item = new HttpClient();
+            $item->setBaseUri($config);
+        }
+
+
+        return DI::set(__METHOD__,  $name, $item);
+    }
+}
